1bb2bae728cbb656f9c4e1d055d3ef7a91e7db66
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-10-28 21:16:32 -0300

    Event Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
 
   constructor() {
     setTimeout(() => {
@@ -20,6 +21,10 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(): void {
+    this.serverCreationStatus = 'Server was created!';
   }
 
 }
