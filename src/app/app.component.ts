57ca7c3cae59f117d32a271390bb4969ea09d7d5
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-11-03 21:31:19 -0300

    Components & Databinding Deep Dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  serverElements = [];
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer(): void {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint(): void {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
 }
