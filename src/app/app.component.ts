e95f23b92f500312255e7a5bc78f9db14266720b
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-11-03 21:50:14 -0300

    Splitting Apps into Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [];
-  newServerName = '';
-  newServerContent = '';
-
-  onAddServer(): void {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-
-  onAddBlueprint(): void {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
 }
